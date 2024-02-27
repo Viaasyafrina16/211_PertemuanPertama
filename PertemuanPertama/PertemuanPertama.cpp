6dc1ae3ff2316dd4c4d24a5d7d58476862312982
Viaasyafrina16 <syafrinavia@gmail.com>
2024-02-27 15:29:40 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //Begin
+  // numeric nAlas, nTinggi, nLuas
+  // Display 'Masukan Tinggi'
+  // Accept nTinggi
+  // Compute nLuas = nAlas*nTinggi/2
+  // Display 'Luasnya = ' + nLuas
+  // End 
+	float nAlas, nTinggi, nLuas;
+	cout << "masukkan Alas = ";
+	cin >> nAlas;
+	cout << "masukkan Tinggi= ";
+	cin >> nTinggi;
+	nLuas = nAlas * nTinggi / 2;
+	cout << "luasnya = " << nLuas;
+
+
+
+
+
+
+}
+
+
+
